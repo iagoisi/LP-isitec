094ec77464f523e20944fc9a9a52f6927945967d
iago isi <iago_isi@hotmail.com>
2021-06-03 14:02:59 -0400

    :heart:  Fetch com API/ POST na lista_01

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 const fields =  document.querySelectorAll('[required]');
 
+const form = document.getElementById('form');
+const url = "https://clientapi.benchmarkemail.com/Contact/18424098/ContactDetails";
+const myHeaders = new Headers();
+myHeaders.append("AuthToken", "830B98D7-CD9E-43B4-BA38-F181169495B1");
+myHeaders.append("Content-Type", "application/json");
+
 function customValidation(event) {
 
     event.preventDefault();
@@ -40,37 +46,37 @@ for ( const field of fields ) {
     field.addEventListener('blur', customValidation)
 }
 
-document.querySelector('form')
-.addEventListener('submit', event => {
-})
+form.addEventListener('submit', event => {
+
+    event.preventDefault();
+
+    let email = document.getElementById('emailInput').value;
+    let data = {
+        "Data": {"Email": `${email}`,
+        "FirstName": null, 
+        "LastName": null, 
+        "EmailPerm": "1"
+        }
+    }    
+
+    fetch ("https://restapi.benchmarkemail.com/Emails/", { 
+        headers: myHeaders,
+    })
+    // .then(response => response.json())
+    // .then(result => console.log(result))
+    // .catch(error => console.log('error', error));
+
+
+    fetch (url, {
+        method: "POST",
+        headers: myHeaders,
+        // redirect: 'follow',
+        body: JSON.stringify(data),
+    })
+    // .then(response => response.json())
+    // .then(result => console.log(result))
+    // .catch(error => console.log('error', error));
 
 
 
-
-
-
-// const btn = document.getElementById('button');
-
-// const intro = document.querySelector('.intro');
-
-// const animation01 = document.querySelector('.animation_01');
-// const animation02 = document.getElementById('animation_02');
-
-// console.log(intro, animation01, animation02)
-
-// function handleClick(){
-//         intro.classList.add('hideIntro');
-//         setTimeout(() => {
-//             animation01.classList.add('openAnimate');
-//             animation01.removeAttribute('animation_01');
-
-//         }, 1100)
-
-
-// }
-
-
-
-
-
-// btn.addEventListener('click', handleClick);
+});
